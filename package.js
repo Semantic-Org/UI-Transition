61154177afb7d468fca0436157fea6cc478bfc85
Pusher Robot <jack@semantic-ui.com>
2015-05-20 11:59:01 -0400

    Updated component to version 1.12.3

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-transition',
   summary : 'Semantic UI - Transition: Single component release',
-  version : '1.12.0',
+  version : '1.12.3',
   git     : 'git://github.com/Semantic-Org/UI-Transition.git',
 });
 
