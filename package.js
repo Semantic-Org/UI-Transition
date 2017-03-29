ee99e23d6261ce790e38aa3710672f2dc0fbc3e0
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:43:26 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-transition',
   summary : 'Semantic UI - Transition: Single component release',
-  version : '2.2.9',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Transition.git',
 });
 
